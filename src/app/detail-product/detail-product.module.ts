77ba2e824b1cdb3e340dd3217bd9703827cdd8c7
Claudio Moreno <clauartur98@gmail.com>
2021-01-28 20:19:18 -0600

    feat: update product list

--- src/app/detail-product/detail-product.module.ts
+++ src/app/detail-product/detail-product.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { DetailProductRoutingModule } from './detail-product-routing.module';
 import { DetailProductComponent } from './detail-product.component';
 import { SharedModule } from '../shared/shared.module';
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
-  declarations: [DetailProductComponent],
+  declarations: [DetailProductComponent, ProductComponent],
   imports: [CommonModule, DetailProductRoutingModule, SharedModule],
 })
 export class DetailProductModule {}
--- src/app/product-list/product-list.module.ts
+++ src/app/product-list/product-list.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { ProductListRoutingModule } from './product-list-routing.module';
 import { ProductListComponent } from './product-list.component';
 import { SharedModule } from '../shared/shared.module';
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
-  declarations: [ProductListComponent],
+  declarations: [ProductListComponent, ProductComponent],
   imports: [CommonModule, ProductListRoutingModule, SharedModule],
 })
 export class ProductListModule {}
