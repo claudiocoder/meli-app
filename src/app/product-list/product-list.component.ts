d0d0be739eb363d2e0654da12dfe45d73b494ee6
Claudio Moreno <clauartur98@gmail.com>
2021-01-31 22:10:01 -0600

    feat: add categories

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -7,12 +7,14 @@ import { ListServiceService } from '../shared/services/list-service.service';
 })
 export class ProductListComponent implements OnInit {
   products: any;
+  categories: any;
 
   constructor(private listService: ListServiceService) {}
 
   ngOnInit(): void {
     this.listService.cart$.subscribe((products: any) => {
       this.products = products.items;
+      this.categories = products.categories;
     });
   }
 }
